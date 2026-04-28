c92da96901ddd495340c6f725cdedc003693044c
youwei1111 <114052158+youwei1111@users.noreply.github.com>
2026-04-28 22:18:09 +0800

    Create config.sample.py

---
+++ python/config.sample.py
@@ -0,0 +1,9 @@
+# 这是一个配置模板，请将其重命名为 config.py
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'YOUR_PASSWORD',
+    'database': 'media_library_system'
+}
+
+TMDB_API_KEY = "YOUR_TMDB_API_KEY_HERE"
